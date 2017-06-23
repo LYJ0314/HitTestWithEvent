13a6f5572949db2ea07200213d7326bc439c3b8f
lyj <lyj@lyjdeimac.local>
2017-06-23 11:25:44 +0800

    Initial Commit

---
+++ HitTestWithEvent/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HitTestWithEvent
+//
+//  Created by lyj on 17/6/23.
+//  Copyright © 2017年 lyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
